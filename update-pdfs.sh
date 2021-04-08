72149fef1aa2c2cb3673668c00ac52f41cd5b939
Ken Bloom <kbloom@gmail.com>
2021-04-07 21:34:54 -0400

    Improvments to update-pdfs.sh
    Identify which binary name to use for musescore. It's called musescore3
    on Debian/Ubuntu, and musescore on Arch/Manjaro.
    
    Fix corrput pdfs with ghostscript.

--- update-pdfs.sh
+++ update-pdfs.sh
@@ -5,7 +5,16 @@ OUTPUT_DIR=$2
 IFS='
 '
 
+MUSESCORE=musescore
+if which musescore3 > /dev/null 2>&1; then
+	MUSESCORE=musescore3
+fi
+
 for x in $(git diff --name-only $SINCE | grep 'mscz$'); do
+	OUTFILE="$OUTPUT_DIR/${x/.mscz/.pdf}"
 	mkdir -p $OUTPUT_DIR/$(dirname $x)
-	musescore3 "$x" -o "$OUTPUT_DIR/${x/.mscz/.pdf}"
+	$MUSESCORE "$x" -o "$OUTFILE"
+	
+	# fix pdfs that are corrupt in ways that xelatex can't handle
+	gs -o - -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress  "$OUTFILE" | sponge "$OUTFILE"
 done
