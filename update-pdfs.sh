631fa2dab6bc4b588767297d946f46d60930c020
Ken Bloom <kbloom@gmail.com>
2025-06-18 16:53:42 -0400

    Change upate-pdfs.sh to work within this directory and automatically detect changed scores.

--- update-pdfs.sh
+++ update-pdfs.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-SINCE=$1
-OUTPUT_DIR=$2
 
-IFS='
-'
 
-MUSESCORE=mscore
-if which musescore3 > /dev/null 2>&1; then
-	MUSESCORE=musescore3
-fi
+shopt -s globstar
 
-for x in $(git diff --name-only $SINCE | grep 'mscz$'); do
-	OUTFILE="$OUTPUT_DIR/${x/.mscz/.pdf}"
-	mkdir -p $OUTPUT_DIR/$(dirname $x)
-	$MUSESCORE "$x" -o "$OUTFILE"
-	
-	# fix pdfs that are corrupt in ways that xelatex can't handle
-	gs -o - -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress  "$OUTFILE" | sponge "$OUTFILE"
+for MSCZ in **/*.mscz ; do
+  PDF="${MSCZ/.mscz/.pdf}"
+  if [[ "$MSCZ" -nt "$PDF" ]]; then
+    echo updating $PDF
+    mscore "$MSCZ" -o "$PDF" && gs -o - -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress  "$PDF" | sponge "$PDF"
+  fi
 done
